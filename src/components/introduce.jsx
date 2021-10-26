847d5d477b0225e6e4adb64c31101cc71b42ebec
S2oonae <S2oonae@gmail.com>
2021-10-26 20:23:32 +0900

    요소 재배열

--- src/components/introduce.jsx
+++ src/components/introduce.jsx
@@ -8,21 +8,30 @@ class Introduce extends PureComponent {
         <div className="contents">
           <img
             className="title-img"
-            src="http://127.0.0.1:8080/img/restaurant.jpg"
+            src="../img/restaurant.jpg"
             alt="restaurant"
           />
           <div className="info">
             <h3 className="restaurant-name">포레스트(For Rest)</h3>
             <p className="restaurant-comment">당신의 휴식을 곁들인 식사</p>
-            <address>
-              <a className="phone" href="tel:+82311234567">
-                031-123-4567
-              </a>
+            <address className="restaurant-contact">
+              <p>
+                <i className="fas fa-phone-alt"></i>
+                <a className="phone" href="tel:+82311234567">
+                  031-123-4567
+                </a>
+              </p>
+              <p className="location">
+                <i className="fas fa-map-marker-alt"></i>
+                경기도 풀잎시 낙엽구 은행로 145
+              </p>
+              <p>
+                <i className="fab fa-instagram"></i>
+                <a className="insta" href="https://www.instagram.com/#">
+                  www.instagram.com/for-rest
+                </a>
+              </p>
             </address>
-            <p className="location">경기도 풀잎시 낙엽구 은행로 145</p>
-            <a className="insta" href="https://www.instagram.com/#">
-              www.instagram.com/for-rest
-            </a>
             <p className="hours">
               Tue ~ Sun <br /> 17:00 ~ 23:00
             </p>
